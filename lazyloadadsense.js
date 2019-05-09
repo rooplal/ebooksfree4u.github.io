3c5dbd2d5bb27cc343ab9a84bcfa964e1e16df6d
rooplal <45243087+rooplal@users.noreply.github.com>
2019-05-09 20:04:43 +0530

    Create lazyloadadsense.js

---
+++ lazyloadadsense.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+// Lazy Load AdSense
+var lazyadsense=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyadsense||0!=document.body.scrollTop&&!1===lazyadsense)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.jsdelivr.net/gh/Arlina-Design/quasar@master/arlinablock.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyadsense=!0)},!0);
+//]]>
